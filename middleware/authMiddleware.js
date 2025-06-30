397693d14caf1f9241bb67705fa7cee7aa6411d8
ttaaarrr <93063822+ttaaarrr@users.noreply.github.com>
2025-06-30 14:29:28 +0700

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const authMiddleware = (req, res, next) => {
+  if (!req.session || !req.session.user) {
+    return res.status(403).json({ message: "กรุณาล็อคอิน" });
+  }
+
+  // กำหนด req.user จาก session.user
+  req.user = {
+    id: req.session.user.id,
+    email: req.session.user.email,
+    fullname: req.session.user.fullname
+  };
+
+  next();
+};
+
+module.exports = authMiddleware;
